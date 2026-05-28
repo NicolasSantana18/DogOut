e1b749381299e286f335885a2da031dfbba0674a
NicolasSantana18 <nicolasnss1107fdn@gmail.com>
2026-05-28 17:04:59 -0300

    Realizando e Testando os métodos GET e DELETE do Springboot no Insomnia com o Banco de Dados MySQL

--- Petshop/src/main/java/com/ProjetoSegundoBimestre/Petshop/controller/controllerProduto.java
+++ Petshop/src/main/java/com/ProjetoSegundoBimestre/Petshop/controller/controllerProduto.java
@@ -4,18 +4,38 @@ package com.ProjetoSegundoBimestre.Petshop.controller;
 import com.ProjetoSegundoBimestre.Petshop.dto.DadosCadastroProduto;
 import com.ProjetoSegundoBimestre.Petshop.model.Produto;
 import com.ProjetoSegundoBimestre.Petshop.repository.ProdutoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin(origins = "*")
-@RequestMapping("produto")
+@RequestMapping("/produto")
 @RestController
 
 public class controllerProduto {
 
+    @Autowired
     private ProdutoRepository produtoRepository;
 
     @PostMapping
-    public void CadastrarProduto(@RequestBody DadosCadastroProduto dados) {
+    @Transactional
+    public void cadastrarProduto(@RequestBody DadosCadastroProduto dados) {
         produtoRepository.save(new Produto(dados));
     }
+
+
+    @GetMapping
+    public List<Produto> listarProdutos(){
+        return produtoRepository.findAll();
+    }
+
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void deletaProduto(@PathVariable Long id) {
+        produtoRepository.deleteById(id);
+    }
+
 }
